df3647f834c6b2159af53edf1a6670fc101ae9d6
Ahsan Feroz <feroz@jongla.com>
2016-01-18 16:48:00 +0200

    wifi class added with 4 variables

---
+++ app/src/main/java/com/arora/comfortsensing/ui/Wifi.java
@@ -0,0 +1,44 @@
+package com.arora.comfortsensing.ui;
+
+/**
+ * Created by ahsanferoz on 18/01/16.
+ */
+public class Wifi {
+
+    private float wifiCapacity;
+    private float wifiSignalStrength;
+    private String wifiAPName;
+    private String wifiIP;
+
+    public float getWifiCapacity() {
+        return wifiCapacity;
+    }
+
+    public void setWifiCapacity(float wifiCapacity) {
+        this.wifiCapacity = wifiCapacity;
+    }
+
+    public float getWifiSignalStrength() {
+        return wifiSignalStrength;
+    }
+
+    public void setWifiSignalStrength(float wifiSignalStrength) {
+        this.wifiSignalStrength = wifiSignalStrength;
+    }
+
+    public String getWifiAPName() {
+        return wifiAPName;
+    }
+
+    public void setWifiAPName(String wifiAPName) {
+        this.wifiAPName = wifiAPName;
+    }
+
+    public String getWifiIP() {
+        return wifiIP;
+    }
+
+    public void setWifiIP(String wifiIP) {
+        this.wifiIP = wifiIP;
+    }
+}
